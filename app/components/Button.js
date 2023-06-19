23b4dd54dc1e8d14b8f661fed0a93580594de49b
Adrian-Kus <you@example.comkustynowicz.ad@gmail.com>
2023-06-19 14:33:08 +0200

    Hero Button mit a Link

--- app/components/Button.js
+++ app/components/Button.js
@@ -1,18 +1,17 @@
-import Link from "next/link";
 import { IoIosArrowForward } from "react-icons/io";
 import style from "@/app/components/Button.module.css";
 
 function Button() {
   return (
     <>
-      <Link href="/contact" scroll={false} className={style.btn}>
+      <a href="/#packages" scroll={false} className={style.btn}>
         <div className={style.btn__text}>
           Jetzt Paket buchen
           <span className={style.btn__text_circle}>
             <IoIosArrowForward className={style.btn__text_icon} />
           </span>
         </div>
-      </Link>
+      </a>
     </>
   );
 }
