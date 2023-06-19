2dd26606bd33649aa2aaaf5f001e3c42daf8bcab
Adrian-Kus <you@example.comkustynowicz.ad@gmail.com>
2023-06-19 04:28:30 +0200

    2 update

--- app/components/Button.js
+++ app/components/Button.js
@@ -5,7 +5,7 @@ import style from "@/app/components/Button.module.css";
 function Button() {
   return (
     <>
-      <Link href="#packages" scroll={false} className={style.btn}>
+      <Link href="/#packages" scroll={false} className={style.btn}>
         <div className={style.btn__text}>
           Jetzt Pakete ansehen
           <span className={style.btn__text_circle}>
