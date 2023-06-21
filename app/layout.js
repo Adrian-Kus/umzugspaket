5e49e854fea4d2b221c0c9287f969f3ebad9f273
Adrian-Kus <you@example.comkustynowicz.ad@gmail.com>
2023-06-21 19:08:07 +0200

    GA instalation

--- app/layout.js
+++ app/layout.js
@@ -1,8 +1,18 @@
+"use client";
 import "./globals.css";
 import Navbar from "@/app/components/Navbar.js";
 import Footer from "@/app/components/Footer.js";
+import { useEffect } from "react";
+import TagManager from "react-gtm-module";
 
 export default function RootLayout({ children }) {
+  const gtmId = process.env.NEXT_PUBLIC_GTM_ID;
+  const tagManagerArgs = { gtmId };
+
+  useEffect(() => {
+    TagManager.initialize(tagManagerArgs);
+  }, []);
+
   return (
     <html lang="de">
       <body>
